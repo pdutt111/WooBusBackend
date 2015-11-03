4a649b4ccbf6d54b22c5f5a40235060ba77596b0
pariskshitdutt <pdutt111>
2015-11-03 11:24:07 +0530

    start and end loc added

--- logic/localServerSyncing.js
+++ logic/localServerSyncing.js
@@ -102,7 +102,7 @@ var syncing={
         busTable.findOne({bus_identifier:req.query.bus_identifier,in_booking:false,is_completed:false,is_deleted:false}
             ,"start end fare discounts departure_time " +
             " distance images total_seats discounted_price route bus_type seats in_transit in_booking is_completed")
-            .populate("route","start end fare distance time_taken active scheduled_stops boarding_points")
+            .populate("route","start end start_loc end_loc fare distance time_taken active scheduled_stops boarding_points")
             .exec()
             .then(function(bus){
                 def.resolve(bus);
