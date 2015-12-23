eb112f5f346973988c83e6e5fbb3e2508140e502
Rohit Gohri <gohri.rohit@gmail.com>
2015-12-23 14:51:32 +0530

    Fixed typo error

--- routes/auditCalls.js
+++ routes/auditCalls.js
@@ -57,7 +57,7 @@ router.get('/protected/audits',
             })
             .catch(function (err) {
                 res.status(err.status).json(err.message);
-            }).done().
+            }).done()
     }
 )
 router.post('/protected/questions',
